3e942b972f151bde04aae957bd93337d6f0df70a
Francesco Bedussi <francesco.bedussi@abstract.it>
2024-05-08 23:08:16 +0200

    chore: remove cypress test components

--- cypress.config.js
+++ cypress.config.js
@@ -4,11 +4,4 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3500',
   },
-
-  component: {
-    devServer: {
-      framework: 'react',
-      bundler: 'vite',
-    },
-  },
 })
